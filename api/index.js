47496ddbd72bad95d15d18601bcec3f0dea1ca1d
ekta <ektabathwal70@gmail.com>
2024-09-29 00:33:24 -0700

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log("error");
+  });
 
 const app = express();
 
