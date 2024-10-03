ecd39891498b80b002513619a6e667a0bd39a0ec
ekta <ektabathwal70@gmail.com>
2024-10-03 11:22:42 -0700

    created signin api route

--- api/routes/Auth.route.js
+++ api/routes/Auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { signup } from "../Controller/Auth.controller.js";
+import { signin } from "../Controller/Auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 export default router;
